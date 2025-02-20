fd8c01965cc7a62b1475c3bded2a19e94979342a
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-02-20 11:24:13 +0300

    Expenses and Percentages

--- NabihaSalary.py
+++ NabihaSalary.py
@@ -1,9 +1,29 @@
-name = input ("Enter your name: ")
-month = input ("Enter the month: ")
+# Function of initsalary
+def initsalary():
+ name = input ("Enter your name: ")
+ month = input ("Enter the month: ")
 
-details = {
+ details = {
     "user": name,
     "month" : month, 
     "salary" : float (input("Enter your salary for the month: "))
 }
-print (details)
+ return details 
+
+# Function to take expense and percentages
+def takeExpenses():
+ expenses= []
+ categories = ["Savings", "Rent" , "Electricity"]
+
+ for category in categories:
+   percent = float (input(f"Enter the percentage allocated to {category}: "))
+
+   expense = {
+    "category": category ,
+    "percentage": percent 
+   }
+
+   expenses.append(expense)
+
+ return expenses
+
