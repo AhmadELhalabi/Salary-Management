9d0b111493da688d2e72d05e26e95b4eb3dd9f9d
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-02-20 11:01:43 +0300

    initial salary

---
+++ NabihaSalary.py
@@ -0,0 +1,9 @@
+name = input ("Enter your name: ")
+month = input ("Enter the month: ")
+
+details = {
+    "user": name,
+    "month" : month, 
+    "salary" : float (input("Enter your salary for the month: "))
+}
+print (details)
